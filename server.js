9bfcf92716d76f6eef3d9d1c9a52152e668b0637
Taieb <ouberka022@gmail.com>
2022-07-27 14:14:29 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+//const fruits = require('./models/fruits.js'); 
+
+app.get("/greeting", (req, res) => {
+    res.send("Hello, stranger");
+});
+
+app.get("/greeting/:name", (req, res) => {
+    res.send("What's up " + req.params.name + " It's so great to see you!");
+});
+app.get("/tip/:total/:tipPercentage/", (req, res) => {
+   // res.send("<h2>Tip Calculator express application created.</h2>");
+    res.send("The tipPercentage is " + req.params.tipPercentage + " % and The total is " + req.params.total + " .");  
+
+});
+//Magic bill
+const magicArray = ["It is certain", "It is decidedly so",
+ "Without a doubt", "Yes definitely","You may rely on it",
+  "As I see it yes", "Most likely", "Outlook good","Yes", 
+  "Signs point to yes", "Reply hazy try again", "Ask again later",
+  "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+  "Don't count on it", "My reply is no", "My sources say no","Outlook not so good",
+   "Very doubtful"];
+   app.get('/magic/:arrayPlay', (req, res) => {
+    let htmlResponse = "";
+for(let i = 0; itr < magicArray.length; i++) {
+    res.send("<h1>" + req.params.arrayPlay[i] + "</h1>");
+}
+   })
+
+
+
+
+
+
+
+
+// server running
+app.listen(port, () => {
+    console.log("The server is running on port " + port);
+})
